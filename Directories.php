60c7d4a5f0b5c9f9572c7e83888f5bfae0cd7fa6
lhj <416703504@qq.com>
2020-03-10 14:21:12 +0800

    目录生成

--- Directories.php
+++ Directories.php
@@ -39,43 +39,41 @@ class Directories
 
     public function gen()
     {
-        $handle = dir($this->rootDir);
-        while ($file = $handle->read()) {
+        $dirArr = scandir($this->rootDir);
+        foreach ($dirArr as $file) {
             if (
-                substr($file, 0, 1) != '.' &&
-                is_dir($this->rootDir . '/' . $file)
+                substr($file, 0, 1) != '.' //上级目录 本级目录 隐藏目录均不显示
+                && is_dir($this->rootDir . '/' . $file
+                )
             ) {
                 $this->tree($this->rootDir . '/' . $file);
             }
         }
 
-        $handle->close();
+//        $handle->close();
     }
 
     private function tree($dir, $code = "")
     {
-        $mydir = dir($dir);
-
-        if (in_array('README.md', scandir($dir))) {
+        $dirArr = scandir($dir);
+        sort($dirArr, SORT_NUMERIC);
+        if (in_array('README.md', $dirArr)) {
             $handle = fopen($dir . '/README.md', 'r');
             $str    = mb_convert_encoding(fgets($handle), 'UTF-8');
             $str    = trim($str, "#\t\n\r\0\x0B");
-//            - [面试题57 - II. 和为s的连续正数序列](./Interview/find-continuous-sequence/)
             fwrite($this->readme, "{$code} - [{$str}]({$dir})\n");
             if (!$handle) {
                 fclose($handle);
             }
         }
 
-        while ($file = $mydir->read()) {
-            if (is_dir($dir . '/' . $file) && $file != '.' && $file != '..') {
-//                echo $code . $dir . '/' . $file . PHP_EOL;
+        foreach ($dirArr as $file) {
+            if (
+                substr($file, 0, 1) != '.' //上级目录 本级目录 隐藏目录均不显示
+                && is_dir($dir . '/' . $file)) {
                 $this->tree($dir . '/' . $file, $code . "\t");
             }
-
         }
-
-        $mydir->close();
     }
 }
 
