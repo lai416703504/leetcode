3dc649a1d6c8bbb90c9db23c380a8f1ced70b678
lhj <416703504@qq.com>
2020-03-10 10:35:01 +0800

    fix

--- Item/543/Version1.php
+++ Item/543/Version1.php
@@ -5,40 +5,42 @@
  * create: 2020/3/10 9:23
  **/
 
+include "../../TreeNode.php";
+
 // Definition for a binary tree node.
-class TreeNode
-{
-    public $val = null;
-    public $left = null;
-    public $right = null;
-
-    function __construct($value)
-    {
-        $this->val = $value;
-    }
-}
-
-function create(array $arr)
-{
-//        $node = self::__construct(array_shift($arr));
-    $queue = new SplQueue();
-    $root  = $node = new TreeNode(array_shift($arr));
-    $queue->enqueue($node);
-    while (!$queue->isEmpty()) {
-        $node = $queue->dequeue();
-        if ($val = array_shift($arr)) {
-            $node->left = new TreeNode($val);
-            $queue->enqueue($node->left);
-        }
-
-        if ($val = array_shift($arr)) {
-            $node->right = new TreeNode($val);
-            $queue->enqueue($node->right);
-        }
-    }
-
-    return $root;
-}
+//class TreeNode
+//{
+//    public $val = null;
+//    public $left = null;
+//    public $right = null;
+//
+//    function __construct($value)
+//    {
+//        $this->val = $value;
+//    }
+//}
+//
+//function create(array $arr)
+//{
+////        $node = self::__construct(array_shift($arr));
+//    $queue = new SplQueue();
+//    $root  = $node = new TreeNode(array_shift($arr));
+//    $queue->enqueue($node);
+//    while (!$queue->isEmpty()) {
+//        $node = $queue->dequeue();
+//        if ($val = array_shift($arr)) {
+//            $node->left = new TreeNode($val);
+//            $queue->enqueue($node->left);
+//        }
+//
+//        if ($val = array_shift($arr)) {
+//            $node->right = new TreeNode($val);
+//            $queue->enqueue($node->right);
+//        }
+//    }
+//
+//    return $root;
+//}
 
 class Solution
 {
---
+++ TreeNode.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * author:lhj
+ * email:416703504@qq.com
+ * create: 2020/3/10 10:32
+ **/
+
+
+class TreeNode {
+     public $val = null;
+     public $left = null;
+     public $right = null;
+     function __construct($value) { $this->val = $value; }
+}
+
+//广度优先插入
+function create(array $arr)
+{
+//        $node = self::__construct(array_shift($arr));
+    $queue = new SplQueue();
+    $root  = $node = new TreeNode(array_shift($arr));
+    $queue->enqueue($node);
+    while (!$queue->isEmpty()) {
+        $node = $queue->dequeue();
+        if ($val = array_shift($arr)) {
+            $node->left = new TreeNode($val);
+            $queue->enqueue($node->left);
+        }
+
+        if ($val = array_shift($arr)) {
+            $node->right = new TreeNode($val);
+            $queue->enqueue($node->right);
+        }
+    }
+
+    return $root;
+}
